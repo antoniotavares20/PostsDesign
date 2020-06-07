383d7f0ce63be7595c1fa154e527a6896f18c6a8
Antonio Tavares Pereira <antoniotavarespereira@gmail.com>
2020-06-07 08:57:07 -0300

    update atualization

--- src/app/posts/posts-create/post-create.component.ts
+++ src/app/posts/posts-create/post-create.component.ts
@@ -29,11 +29,7 @@ export class PostCreateComponent{
 ngOnInit(){
   this.form = new FormGroup({
     'title' : new FormControl(null, {validators:[Validators.required, Validators.minLength(3)]}),
-    'content': new FormControl(null, {
-      validators: [Validators.required]
-    }),
-    'image' : new FormControl(null,{validators: [Validators.required]})
-
+    'content': new FormControl(null, {validators: [Validators.required]})
   });
   this.router.paramMap.subscribe((paramMap: ParamMap)=>{
       if(paramMap.has('postId')){
@@ -64,7 +60,7 @@ ngOnInit(){
   onImagePicked(event: Event){
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({image: file});
-    this.form.get('image').updateValueAndValidity();
+    this.form.get('image').updateValueAndValidity;
     const reader = new FileReader();
     reader.onload = ()=>{
       this.imagePreview = reader.result as string;
@@ -90,18 +86,11 @@ ngOnInit(){
               this.postId,
               this.form.value.title,
               this.form.value.content
-              );
 
+              );
+              this.form.reset();
       }
 
-      this.form.reset();
-      //     const post =  {
- //         title: form.value.title,
-       //   content: form.value.content
-     //  };
 
-       //objeto que recebe o post
-     //  this.postCreated.emit(post);
-     //  console.log(this.postCreated);
     }
 }
